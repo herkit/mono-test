e14bc45cecb6961092a8cfb98fa418c3ca865e22
Henrik Grotle <henrik@pswin.com>
2014-10-23 23:26:34 +0200

    queue and get from local rabbitmq

---
+++ 02-queuing/QueueIt/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            var name = Console.ReadLine();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    string message = "Hello " + name;
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "hello", null, body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+        }
+    }
+}
